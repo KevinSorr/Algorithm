6ab0cf785a973800322100930a2e3d41a90c4968
yindemin <821180585@qq.com>
2017-06-07 15:21:09 +0800

    二分查找修改区间
    Change-Id: Id0d261b0c49cd5715fb1e5467d4aaa6dcb693b25

---
+++ A.数组/BinarySearch/BinarySearch2.java
@@ -0,0 +1,38 @@
+
+/**
+ * Created by ydm on 2017/6/7.
+ * 二分查找：
+ *      对于有序数列  常采用此方法
+ *
+ * 注意：处理边界与索引指向问题
+ *
+ * 更改左右区间
+ */
+public class BinarySearch2<T> {
+
+    public int binarySearch(T[] array,int n,T target){
+        int left = 0,right = n ;//设置区间为[left,right]
+        while (left < right){   //当left == right 时,[left,right)仍然为无效区间
+            int mid = left + right / 2;
+            if(target == array[mid]){
+                return mid;
+            }
+            if((Integer)target < (Integer) array[mid]){
+                right = mid;
+            }
+            if((Integer)target >(Integer) array[mid]){
+                left = mid + 1;
+            }
+
+        }
+
+        return  -1;
+    }
+
+    public static void main(String[] args) {
+        Integer[] arrays = {1,3,4,6,7};
+        int value = new BinarySearch2<Integer>().binarySearch(arrays,arrays.length,3);
+        System.out.print(value);
+
+    }
+}
